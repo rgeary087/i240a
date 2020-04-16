0caef2602583309ee603dae113e2429e7ed0c6ea
Ryan Geary <rgeary1@cs.binghamton.edu>
2020-04-16 17:38:25 -0400

    completing lab10

---
+++ submit/lab10/exercises/2-tdd/digit-sum.cc
@@ -0,0 +1,33 @@
+#include <cctype>
+
+#include "digit-sum.hh"
+
+int
+digitSum(const char* str)
+{
+	int acc = 0;
+	bool wantDigit = true;
+	for(const char* p = str; *p; ++p){
+		int c = *p;
+		if(isdigit(c)){
+			if(wantDigit){
+				acc += c -'0';
+				wantDigit = false;
+			}
+			else{
+				break;
+			}
+		}
+		else if(c == '+'){
+			if(wantDigit) break;
+			wantDigit = true;
+		}
+		else if(isspace(c)){
+			continue;
+		}
+		else{
+			break;
+		}
+	}
+	return acc;
+}
